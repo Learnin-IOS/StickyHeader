0c05491adcf01e1bc77ae539ce3c365a9637bc4f
Sharkzuria <baumabulondo@gmail.com>
2022-12-03 03:32:05 +0300

    Initial Commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Le Bon B' Bauma on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
