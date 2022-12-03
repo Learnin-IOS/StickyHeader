3aafa58170c9e996fb86736531632dd46d2a1464
Sharkzuria <baumabulondo@gmail.com>
2022-12-03 04:20:22 +0300

    style: Add Home View

---
+++ StickyHeader/View/Home.swift
@@ -0,0 +1,25 @@
+//
+//  Home.swift
+//  StickyHeader
+//
+//  Created by Le Bon B' Bauma on 03/12/2022.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    // MARK: - Properties
+    
+    var safeArea: EdgeInsets
+    var size: CGSize
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
