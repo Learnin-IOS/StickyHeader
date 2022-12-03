72b43d10afa6a12fe452fc327fceeeeac4983a11
Sharkzuria <baumabulondo@gmail.com>
2022-12-03 04:20:46 +0300

    feat: Add Album and Sample Data

--- StickyHeader/Model/Album.swift
+++ StickyHeader/Model/Album.swift
@@ -7,3 +7,38 @@
 
 import SwiftUI
 
+
+// MARK: - Album Model and Smaple Data
+
+struct Album: Identifiable{
+    var id = UUID().uuidString
+    var albumName: String
+    
+}
+
+
+var albums: [Album] = [
+
+    Album(albumName: "Bloqué"),
+    Album(albumName: "Fleur Froide - Second état : la cristalisation"),
+    Album(albumName: "Se Yo"),
+    Album(albumName: "Destin"),
+    Album(albumName: "Tokooos II"),
+    Album(albumName: "Tokooos II Gold"),
+    Album(albumName: "Science - Fiction"),
+    Album(albumName: "Strandje Aan De Maas"),
+    Album(albumName: "Inama"),
+    Album(albumName: "Par Terre - A COLOR SHOW"),
+    Album(albumName: "QALF infinity"),
+    Album(albumName: "Berna Reloaded"),
+    Album(albumName: "Flavour of Africa"),
+    Album(albumName: "Control"),
+    Album(albumName: "Gentleman 2.0"),
+    Album(albumName: "Droit Chemin"),
+    Album(albumName: "Arsenal des belles mélodies"),
+    Album(albumName: "Power 'Kosa Leka' : Vol 1"),
+    Album(albumName: "Historia"),
+    Album(albumName: "Tokooos"),
+    
+    
+]
