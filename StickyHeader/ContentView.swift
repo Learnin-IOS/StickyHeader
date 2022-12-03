06f19a44c079fd97194c1c7772b033c79f625039
Sharkzuria <baumabulondo@gmail.com>
2022-12-03 04:20:05 +0300

    feat: Implement Content View

--- StickyHeader/ContentView.swift
+++ StickyHeader/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader {
+            let safeArea = $0.safeAreaInsets
+            let size = $0.size
+            Home(safeArea: safeArea, size: size)
         }
-        .padding()
+        .preferredColorScheme(.dark )
     }
 }
 
